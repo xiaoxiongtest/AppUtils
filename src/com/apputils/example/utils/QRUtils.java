03bcc5bd92322a9d276cf21b83ea06b0d20cc97f
xiaoxiaoxiong <1175670228@qq.com>
2016-01-05 15:22:32 +0800

    生成带logo的二维码

--- src/com/apputils/example/utils/QRUtils.java
+++ src/com/apputils/example/utils/QRUtils.java
@@ -9,12 +9,20 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 /**
  * 生成二维码或者扫描二维码
  */
 public class QRUtils {
-	// 要转换的地址或字符串,可以是中文
+	/**
+	 * 生成二维码
+	 * 
+	 * @param url
+	 * @param width
+	 * @param height
+	 * @return
+	 */
 	public static Bitmap createQRImage(String url, int width, int height) {
 		try {
 			// 判断URL合法性
@@ -43,8 +51,65 @@ public class QRUtils {
 			return bitmap;
 		} catch (WriterException e) {
 			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 添加带logo的二维码
+	 * 
+	 * @param url
+	 * @param width
+	 * @param height
+	 * @param bit
+	 * @return
+	 */
+	public static Bitmap createQRImageWithLogo(String url, int width, int height, Bitmap bit) {
+		Bitmap qr = createQRImage(url, width, height);
+		if (bit == null) {
+			return qr;
+		} else {
+			return addLogo(qr, bit);
+		}
+	}
+
+	/**
+	 * 在二维码中间添加Logo图案
+	 */
+	private static Bitmap addLogo(Bitmap src, Bitmap logo) {
+		if (src == null) {
 			return null;
 		}
+		if (logo == null) {
+			return src;
+		}
+		// 获取图片的宽高
+		int srcWidth = src.getWidth();
+		int srcHeight = src.getHeight();
+		int logoWidth = logo.getWidth();
+		int logoHeight = logo.getHeight();
+		if (srcWidth == 0 || srcHeight == 0) {
+			return null;
+		}
+		if (logoWidth == 0 || logoHeight == 0) {
+			return src;
+		}
+		// logo大小为二维码整体大小的1/5
+		float scaleFactor = srcWidth * 1.0f / 5 / logoWidth;
+		Bitmap bitmap = Bitmap.createBitmap(srcWidth, srcHeight, Bitmap.Config.ARGB_8888);
+		try {
+			Canvas canvas = new Canvas(bitmap);
+			canvas.drawBitmap(src, 0, 0, null);
+			canvas.scale(scaleFactor, scaleFactor, srcWidth / 2, srcHeight / 2);
+			canvas.drawBitmap(logo, (srcWidth - logoWidth) / 2, (srcHeight - logoHeight) / 2, null);
+
+			canvas.save(Canvas.ALL_SAVE_FLAG);
+			canvas.restore();
+		} catch (Exception e) {
+			bitmap = null;
+			e.getStackTrace();
+		}
+		return bitmap;
 	}
 
 }
